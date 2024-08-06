4412377274a385586fc2a657328ec349f3e62fce
Preeti <preetiyadav5443@gmail.com>
2024-08-06 16:53:16 +0530

    basic setup done

---
+++ components/sections/SectionHero.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useTranslate } from '#imports';
+
+const t = useTranslate()
+defineProps<{
+    title: { en: string }
+    subtitle: { en: string }
+}>()
+</script>
+
+<template>
+    <section class="px-4 py-12">
+        <h1 class="mb-2 font-semibold text-3xl uppercase">{{ t(title) }}</h1>
+        <h2>{{ t(subtitle) }}</h2>
+    </section>
+</template>
