2d27978f7b9c26c5c95584c05877fe50a566a42f
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-08-08 10:23:39 +0530

    added faqs

---
+++ components/sections/SectionFaqs.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useTranslate } from "#imports"
+import InputV from "../common/InputV.vue"
+
+const t = useTranslate()
+defineProps<{
+    title: { en: string }
+    subtitle: { en: string }
+}>()
+</script>
+
+<template>
+    <section>
+        <div class="bg-white-light py-20">
+            <div class="container mx-auto px-5 max-w-[1320px]">
+                <h2 class="font-extrabold text-black leading-110 text-center text-6xxl">{{ t(title) }}</h2>
+            </div>
+        </div>
+    </section>
+</template>
