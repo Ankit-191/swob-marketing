bcd7a1ac19d68c6d71f100ff108672c1fac6d401
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-08-08 16:48:18 +0530

    update feature section

--- components/sections/SectionFeature.vue
+++ components/sections/SectionFeature.vue
@@ -35,7 +35,7 @@ defineProps<{
     <section class="py-12 sm:py-14 md:py-16 lg:py-20">
         <div class="container mx-auto px-4 sm:px-5 max-w-[1300px]">
             <h2
-                class="font-semibold font-sans sm:font-extrabold max-w-[768px] text-center mx-auto xl:max-w-[662px] text-black-gray !leading-110 text-[30px] sm:text-4xl md:text-5xl lg:text-5xxl">
+                class="font-semibold font-sans sm:font-extrabold max-w-[768px] text-center mx-auto xl:max-w-[662px] text-gray-dark !leading-110 text-[30px] sm:text-4xl md:text-5xl lg:text-5xxl">
                 {{ t(title) }}</h2>
             <p
                 class="font-sans text-xl !leading-150 text-center mx-auto text-black-light w-full md:max-w-[768px] pt-4 sm:pt-5">
