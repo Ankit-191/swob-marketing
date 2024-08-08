86bb732a02a598fb2efb096ac70bbd5a53a36ee9
Ankit-191 <godaraankit003@gmail.com>
2024-08-08 17:00:21 +0530

    meta image add

--- app.vue
+++ app.vue
@@ -21,6 +21,45 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
     />
+    <!-- Primary Meta Tags -->
+    <title>Revolutioniere dein HR-Managementnerate</title>
+    <meta name="title" content="Revolutioniere dein HR-Managementnerate" />
+    <meta
+      name="description"
+      content="Eine HR-Software für jede Unternehmensgröße. Bleib über unsere Einführung auf dem Laufenden."
+    />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://swob-marketing-main.vercel.app/" />
+    <meta
+      property="og:title"
+      content="Revolutioniere dein HR-Managementnerate"
+    />
+    <meta
+      property="og:description"
+      content="Eine HR-Software für jede Unternehmensgröße. Bleib über unsere Einführung auf dem Laufenden."
+    />
+    <meta
+      property="og:image"
+      content="https://swob-marketing-main.vercel.app/assets/images/png/meta-tags.png"
+    />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://swob-marketing-main.vercel.app/" />
+    <meta
+      property="twitter:title"
+      content="Revolutioniere dein HR-Managementnerate"
+    />
+    <meta
+      property="twitter:description"
+      content="Eine HR-Software für jede Unternehmensgröße. Bleib über unsere Einführung auf dem Laufenden."
+    />
+    <meta
+      property="twitter:image"
+      content="https://swob-marketing-main.vercel.app/assets/images/png/meta-tags.png"
+    />
   </Head>
 
   <NuxtLoadingIndicator />
